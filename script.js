692b0a0e5f88dc32f6bae22fa8f4b42f0612c671
GabrielLimaTG <57038728+GabrielLimaTG@users.noreply.github.com>
2023-01-19 21:27:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
+//this selects the class .pic
 const picture = document.querySelector('.pic');
 
-const changePic = () => picture.classList.toggle('newImg');
-
-
+//this func generates a random number from 0 to 999999 and adds a # to it: e.g #548972
 const randomColor = () => {
-
 num = Math.floor(Math.random()*999999);
 return num = '#'+num;
 }
 
-
+//this func runs when the Event (click) is observed, it changes the BG color with and gets the number of the palette set by the previous function
 const changeBack = () => picture.style.backgroundColor = randomColor();
 
-
+//This observes if the event happens and calls the previous function
 picture.addEventListener('click', changeBack);
